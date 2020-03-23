6c84c1c0dd17e2fe677e0b464a0826e9dd3e3d03
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-03-23 16:09:05 +0530

    design change + bug fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const burger = document.querySelector('.burger');
 const navbar = document.getElementById('navbar');
 const links = document.querySelectorAll('.links');
 let mode=1;
-const width  = window.innerWidth || document.documentElement.clientWidth || 
+const windowWidth  = window.innerWidth || document.documentElement.clientWidth || 
 document.body.clientWidth;
 const showMenu = () => {
      burger.addEventListener('click', () => {
@@ -18,7 +18,7 @@ const showMenu = () => {
 }
 links.forEach(item => {
     item.addEventListener('click', () => {
-        if(width < 400){
+        if(windowWidth < 500){
             navbar.style.height = '10vh';
             mode=1;
         }
