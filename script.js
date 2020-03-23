32a0ef8961321d7af6b86d8e8bde4e0becab6d9a
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-03-23 12:46:39 +0530

    bug fix

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const burger = document.querySelector('.burger');
 const navbar = document.getElementById('navbar');
 const links = document.querySelectorAll('.links');
-console.log(navbar);
 let mode=1;
+function is_mobile() {
+    var agents = ['android', 'webos', 'iphone', 'ipad', 'blackberry'];
+    for(i in agents) {
+        if(navigator.userAgent.match('/'+agents[i]+'/i')) {
+            return true;
+        }
+    }
+    return false;
+}
 const showMenu = () => {
      burger.addEventListener('click', () => {
          if(mode==1){
@@ -17,7 +25,7 @@ const showMenu = () => {
 }
 links.forEach(item => {
     item.addEventListener('click', () => {
-        if(screen.width < 700){
+        if(!is_mobile){
             navbar.style.height = '10vh';
             mode=1;
         }
