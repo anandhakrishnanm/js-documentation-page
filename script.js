08c825c6c43147c147c9cc86821334022745ca5f
anandhakrishnanm <anandhakrishnanmohank@gmail.com>
2020-03-23 11:27:59 +0530

    design change

---
+++ script.js
@@ -0,0 +1,25 @@
+const burger = document.querySelector('.burger');
+const navbar = document.getElementById('navbar');
+const links = document.querySelectorAll('.links');
+console.log(navbar);
+let mode=1;
+const showMenu = () => {
+     burger.addEventListener('click', () => {
+         if(mode==1){
+            navbar.style.height = '100vh';
+            mode=0;
+         }
+         else{
+            navbar.style.height = '10vh';
+            mode=1;
+         }
+     }); 
+}
+links.forEach(item => {
+    item.addEventListener('click', () => {
+        navbar.style.height = '10vh';
+        mode=1;
+    });
+});
+
+showMenu();
